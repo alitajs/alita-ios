3c9b82d9fea8de688f9f1f3636abf56160672f95
xiaohuoni <448627663@qq.com>
2020-11-06 15:05:30 +0800

    feat: 不提交 Pods

--- Pods/Target Support Files/Pods-miniapp/Pods-miniapp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_miniapp : NSObject
-@end
-@implementation PodsDummy_Pods_miniapp
-@end
--- Pods/Target Support Files/Pods-miniappTests/Pods-miniappTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_miniappTests : NSObject
-@end
-@implementation PodsDummy_Pods_miniappTests
-@end
